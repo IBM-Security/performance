30dc11a5d1a1cb6e504575a202811359556fb653
Dave Bachmann <bachmann@us.ibm.com>
2019-05-24 22:12:39 -0500

    Update process_javacores.sh to handle directory names including javacore.

--- IAM/tools/javacores/gen_threadstacks.sh
+++ IAM/tools/javacores/gen_threadstacks.sh
@@ -1,10 +1,11 @@
 PROG_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
-sourcefile=$1
+sourcefile=basename $1
+sourcedir=dirname $1
 targetfile=`echo $1 | sed 's/javacore/threadstacks/'`
 longstackfile=`echo $1 | sed 's/javacore/longstacks/'`
 
-perl ${PROG_DIR}/j9dumpsummary.pl $sourcefile | sort --key=3 > $targetfile
-awk '{if (length($0) > 1000) print}' $targetfile  > $longstackfile
-${PROG_DIR}/stackcollapse-threadstack.sh $targetfile | ${PROG_DIR}/flamegraph.pl > ${targetfile}.svg
-${PROG_DIR}/stackcollapse-threadstack.sh $longstackfile | ${PROG_DIR}/flamegraph.pl > ${longstackfile}.svg
+perl ${PROG_DIR}/j9dumpsummary.pl $sourcedir/$sourcefile | sort --key=3 > $sourcedir/$targetfile
+awk '{if (length($0) > 1000) print}' $sourcedir/$targetfile  > $sourcedir/$longstackfile
+${PROG_DIR}/stackcollapse-threadstack.sh $sourcedir/$targetfile | ${PROG_DIR}/flamegraph.pl > $sourcedir/${targetfile}.svg
+${PROG_DIR}/stackcollapse-threadstack.sh $sourcedir/$longstackfile | ${PROG_DIR}/flamegraph.pl > $sourcedir/${longstackfile}.svg
