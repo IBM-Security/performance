7f3cfd0c19aac7163efca0a58318fb004215dc63
Dave Bachmann <bachmann@us.ibm.com>
2019-07-02 07:34:17 -0500

    Add script to temporarily enable monitoring.

---
+++ IAM/scripts/ISIM_tuning_scripts/perftune_updatemonitoring.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+db2 update monitor switches using STATEMENT ON
+db2 update monitor switches using BUFFERPOOL ON
+db2 update monitor switches using LOCK ON
+db2 update monitor switches using SORT ON
+db2 update monitor switches using TIMESTAMP ON
+db2 update monitor switches using UOW ON
