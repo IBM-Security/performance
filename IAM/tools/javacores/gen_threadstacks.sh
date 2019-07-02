4b95a1e2a745a1b16f62a94c1151a214418c19a8
Dave Bachmann <bachmann@us.ibm.com>
2019-07-01 22:37:52 -0500

    Fix support for processing subdirectories.

--- IAM/tools/javacores/gen_threadstacks.sh
+++ IAM/tools/javacores/gen_threadstacks.sh
@@ -1,11 +1,10 @@
-PROG_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+PROG_DIR=$(cd $(dirname $0) && pwd)
 
-sourcefile=basename $1
-sourcedir=dirname $1
+sourcefile=$1
 targetfile=`echo $1 | sed 's/javacore/threadstacks/'`
 longstackfile=`echo $1 | sed 's/javacore/longstacks/'`
 
-perl ${PROG_DIR}/j9dumpsummary.pl $sourcedir/$sourcefile | sort --key=3 > $sourcedir/$targetfile
-awk '{if (length($0) > 1000) print}' $sourcedir/$targetfile  > $sourcedir/$longstackfile
-${PROG_DIR}/stackcollapse-threadstack.sh $sourcedir/$targetfile | ${PROG_DIR}/flamegraph.pl > $sourcedir/${targetfile}.svg
-${PROG_DIR}/stackcollapse-threadstack.sh $sourcedir/$longstackfile | ${PROG_DIR}/flamegraph.pl > $sourcedir/${longstackfile}.svg
+perl ${PROG_DIR}/j9dumpsummary.pl $sourcefile | sort --key=3 > $targetfile
+awk '{if (length($0) > 1000) print}' $targetfile  > $longstackfile
+${PROG_DIR}/stackcollapse-threadstack.sh $targetfile | /home/bachmann/src/FlameGraph/flamegraph.pl > ${targetfile}.svg
+${PROG_DIR}/stackcollapse-threadstack.sh $longstackfile | /home/bachmann/src/FlameGraph/flamegraph.pl > ${longstackfile}.svg
--- IAM/tools/javacores/process_javacores.sh
+++ IAM/tools/javacores/process_javacores.sh
@@ -4,17 +4,21 @@ if [ $# -lt 1 ]; then
    exit
 fi
 
-if [ ! -f $1/javacore*.txt ]; then
+filelist=`ls $1/javacore*.txt`
+
+if [ "X${filelist}" == "X" ]; then
    echo no javacores found in $1
    exit
 fi
 
-PROG_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+PROG_DIR=$(cd $(dirname $0) && pwd)
 
-for sourcefile in `ls $1/javacore*.txt`
+cd $1
+
+for sourcefile in `ls javacore*.txt`
 do
    ${PROG_DIR}/gen_threadstacks.sh $sourcefile
 done
 
-awk '{print $NF}' $1/longstack*.txt | sort > $1/all_longstacks.txt
-${PROG_DIR}/stackcollapse-threadstack.sh $1/all_longstacks.txt | ${PROG_DIR}/flamegraph.pl > $1/all_longstacks.txt.svg
+awk '{print $NF}' longstack*.txt | sort > all_longstacks.txt
+${PROG_DIR}/stackcollapse-threadstack.sh all_longstacks.txt | ${PROG_DIR}/flamegraph.pl > all_longstacks.txt.svg
